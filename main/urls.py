82903980344fd96d2d7bb48e8d6e85d2c814b305
Murloc Knight <murlockkinght@gmail.com>
2022-04-03 14:25:14 +0700

    added main page and profile. added login and register button. also replace hardcoded username to templates.

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("profile", TemplateView.as_view(template_name="profile.html"), name="profile"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
