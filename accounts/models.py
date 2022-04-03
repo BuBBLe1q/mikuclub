e7c414401428e4c28540fd75c190000327bd2a0a
BuBBLe1q <zyka1803@gmail.com>
2022-04-03 22:12:13 +0700

    Merge with murloc_branch
    From here - https://github.com/KnightMurloc/mikuclub

---
+++ accounts/models.py
@@ -0,0 +1,69 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.mail import send_mail
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
+from django.db import models
+from django.utils import timezone
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+
+    Username and password are required. Other fields are optional.
+    """
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField("email address", blank=False)
+    is_staff = models.BooleanField(
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    is_active = models.BooleanField(
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+# class CustomUser(AbstractUser):
+#     pass
+#
+#     # add additional fields in here
+#
+#     def __str__(self):
+#         return self.username
---
+++ readme.txt
@@ -0,0 +1,9 @@
+для запуска
+
+python -m venv venv
+для windows
+venv\Scripts\Activate.ps1
+для Linux
+source ./venv/bin/activate
+pip install -r requirements.txt
+python manage.py runserver
