c60a3cc7caaa4cf0af71686b97e1eed900a2340f
BuBBLe1q <zyka1803@gmail.com>
2022-04-22 21:32:46 +0700

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
 
-urlpatterns = [
-    # ... 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns = [
+#     # ... 
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
